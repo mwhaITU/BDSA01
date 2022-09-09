91cc2c42976603d1a5e7173c791a7c8d6bd386f6
Jklo <jklo@itu.dk>
2022-09-09 13:49:03 +0200

    Flatten being implemented and tested

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,5 +1,23 @@
 namespace Assignment1.Tests;
+using Xunit;
+using FluentAssertions;
 
 public class IteratorsTests
 {
+
+    [Fact]
+    public void flatten_should_return_single_stream_of_t()
+    {
+        //Arrange
+        var innerlist1 = new List<int>() { 1, 2, 3, 4, 5 };
+        var innerlist2 = new List<int>() { 1, 2, 3, 4, 5 };
+        var list1 = new List<IEnumerable<int>>() { innerlist1, innerlist2 };
+
+        //Act
+        var actual = Iterators.Flatten(list1);
+        //Assert
+        Console.WriteLine(actual);
+        
+        
+    }
 }
