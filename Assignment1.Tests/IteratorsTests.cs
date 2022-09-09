afc080d6ab70b40e2ea2e12ff6b0937750e7e295
Jklo <jklo@itu.dk>
2022-09-09 14:01:25 +0200

    Flatten implemented and tested

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -17,6 +17,5 @@ public class IteratorsTests
         var actual = Iterators.Flatten(list1);
         //Assert
         actual.Should().BeEquivalentTo(new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 });
-
     }
 }
