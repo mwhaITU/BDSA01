451ff540f9ccc3c6751523ae7c98e1b6bd254e5d
Martin <mwha@itu.dk>
2022-09-09 13:49:06 +0200

    First iteration of implementation of Filter

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,5 +1,11 @@
 namespace Assignment1.Tests;
 
+using Xunit;
+
 public class IteratorsTests
 {
+    [Fact]
+    public void Filter_returns_2_and_4_when_given_12345_with_even_predicate() {
+        Iterators.Filter<int> filter = new Iterators.Filter<int>();
+    }
 }
--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -4,5 +4,11 @@ public static class Iterators
 {
     public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
 
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) {
+        foreach(T item in items) {
+            if(predicate(item)) {
+                yield return item;
+            }
+        }
+    }
 }
