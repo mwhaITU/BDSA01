9f67769b66beed0cd28ddb6d06369b5caf31b0f7
Martin <mwha@itu.dk>
2022-09-09 13:57:18 +0200

    Implemented filter function and its relative test

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,11 +1,19 @@
 namespace Assignment1.Tests;
-
 using Xunit;
 
 public class IteratorsTests
 {
     [Fact]
-    public void Filter_returns_2_and_4_when_given_12345_with_even_predicate() {
-        Iterators.Filter<int> filter = new Iterators.Filter<int>();
+    public void filter_should_return_2_and_4_given_12345_with_even_predicate() {
+        // Arrange
+        var list = new List<int>() { 1, 2, 3, 4, 5 };
+        Predicate<int> even = Even;
+        bool Even(int i) => i % 2 == 0;
+
+        // Act
+        var actual = Iterators.Filter<int>(list, even);
+
+        // Assert
+        Assert.Equal(new List<int> { 2, 4 }, actual);
     }
 }
