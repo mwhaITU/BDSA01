d190a908bf8aca2f57b6d2cf11c4c233c2b39526
Jklo <jklo@itu.dk>
2022-09-13 12:15:46 +0200

    Fixed small syntax error

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -18,10 +18,11 @@ public class IteratorsTests
         //Assert
         actual.Should().BeEquivalentTo(new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 });
 
-public class IteratorsTests
-{
+    }
+
     [Fact]
-    public void filter_should_return_2_and_4_given_12345_with_even_predicate() {
+    public void filter_should_return_2_and_4_given_12345_with_even_predicate()
+    {
         // Arrange
         var list = new List<int>() { 1, 2, 3, 4, 5 };
         Predicate<int> even = Even;
@@ -32,6 +33,5 @@ public class IteratorsTests
 
         // Assert
         Assert.Equal(new List<int> { 2, 4 }, actual);
-
     }
 }
